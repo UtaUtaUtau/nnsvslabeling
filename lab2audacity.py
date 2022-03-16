1bd605e175e1dc3e2eeca17853d54f5883bb61dc
UtaUtaUtau <jmctayag@gmail.com>
2022-03-16 10:39:17 +0800

    Update lab2audacity.py
    i just made it print out errors. maybe track it down better too but idk.

--- lab2audacity.py
+++ lab2audacity.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import traceback
 
 floc = sys.argv[-1]
 base, ext = os.path.splitext(floc)
@@ -11,26 +12,35 @@ def groups(iterable, num):
             res.append(iterable[i + j])
         yield tuple(res)
 
-if ext == '.lab':
-    lab = open(floc).readlines()
-    with open(base + '.txt', 'w') as f:
-        for i in lab:
-            time = i.strip().split()
-            if '.' in time[0]:
-                f.write('\t'.join(time) + '\n')
-            else:
-                f.write(f'{float(time[0]) / (10 ** 7):.6f}\t{float(time[1]) / (10 ** 7):.6f}\t{time[2]}\n')
-elif ext == '.txt':
-    aud = open(floc).readlines()
-    for i in range(len(aud) - 1, -1, -1):
-        if aud[i].startswith('\\'):
-            del aud[i]
-            
-    with open(base + '.lab', 'w') as f:
-        for i, j in groups(aud, 2):
-            time1 = i.strip().split()
-            time2 = j.strip().split()
-            f.write(f'{int(float(time1[0]) * (10 ** 7))} {int(float(time2[0]) * (10 ** 7))} {time1[2]}\n')
-        if not aud[-1].strip().endswith('-'):
-            time = aud[-1].strip().split()
-            f.write(f'{int(float(time[0]) * (10 ** 7))} {int(float(time[1]) * (10 ** 7))} {time[2]}\n')
+try:
+    if ext == '.lab':
+        lab = open(floc).readlines()
+        with open(base + '.txt', 'w') as f:
+            for i in lab:
+                time = i.strip().split()
+                if '.' in time[0]:
+                    f.write('\t'.join(time) + '\n')
+                else:
+                    f.write(f'{float(time[0]) / (10 ** 7):.6f}\t{float(time[1]) / (10 ** 7):.6f}\t{time[2]}\n')
+    elif ext == '.txt':
+        aud = open(floc).readlines()
+        for i in range(len(aud) - 1, -1, -1):
+            if aud[i].startswith('\\'):
+                del aud[i]
+                
+        with open(base + '.lab', 'w') as f:
+            for i, j in groups(aud, 2):
+                time1 = i.strip().split()
+                time2 = j.strip().split()
+                f.write(f'{int(float(time1[0]) * (10 ** 7))} {int(float(time2[0]) * (10 ** 7))} {time1[2]}\n')
+            if not aud[-1].strip().endswith('-'):
+                time = aud[-1].strip().split()
+                f.write(f'{int(float(time[0]) * (10 ** 7))} {int(float(time[1]) * (10 ** 7))} {time[2]}\n')
+except Exception as e:
+    for err in traceback.format_exception(e.__class__, e, e.__traceback__):
+        print(err, end='')
+    if e.__class__.__name__ == 'IndexError':
+        print('Probably a label without a phoneme in it? Here:\n')
+        print(i)
+        print('Search it in the audacity or HTS mono label you put in.')
+    os.system('pause')

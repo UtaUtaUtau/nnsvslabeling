39b310e530d6989b35588eaa82928ddc67dad210
UtaUtaUtau <jmctayag@gmail.com>
2022-06-15 19:12:39 +0800

    Added phoneme... "density"
    It counts how much of each phonemes is in there. You can pretty much replace the `if v == 0:` part to be a threshold if you think just having an instance of a phoneme is not ok.

--- README.md
+++ README.md
@@ -18,6 +18,8 @@ It's exactly what the file says. It checks for wrong phonemes in every .lab file
 
 It checks Japanese labels by default, but if you want to change what language it checks, just open it up and change the `phones` list to the phoneme list you want it to be.
 
+Update 06/15/2022: Added a feature that counts how many instances of each phoneme is present in the labels. The program will alert you if there is no instance of a certain phoneme by default but you may change it to be a certain threshold. At line 28 in the code (looks like `if v == 0:`) you may change it to something like `if v <= n:` with `n` being a threshold that you think is appropriate enough.
+
 ### lab2ust
 The whole lab2ust folder will be put in the UTAU plugins folder.
 
--- check_labels.py
+++ check_labels.py
@@ -2,6 +2,11 @@ import glob
 import os
 phones = ['br', 'a', 'i', 'u', 'e', 'o', 'N', 'cl', 't', 'd', 's', 'sh', 'j', 'z', 'ts', 'k', 'kw', 'g', 'gw', 'h', 'b', 'p', 'f', 'ch', 'ry', 'ky', 'py', 'dy', 'ty', 'ny', 'hy', 'my', 'gy', 'by', 'n', 'm', 'r', 'w', 'v', 'y', 'pau', 'Edge']
 
+density = {}
+
+for i in phones:
+    density[i] = 0
+
 for file in glob.glob('*.lab'):
     with open(file) as f:
         i = f.readline()
@@ -11,9 +16,16 @@ for file in glob.glob('*.lab'):
             i = f.readline()
             if p[-1] not in phones:
                 print(f'In {file}: {p[-1]} is not in the list. At line {line_count}')
+            else:
+                density[p[-1]] += 1
 
             if int(p[0]) == int(p[1]):
                 print(f'In {file}: {p[-1]} has the same start and end. At line {line_count}')
+
             line_count += 1
+
+for k, v in density.items():
+    if v == 0:
+        print(f'No data for {k}')
 
 os.system('pause')
